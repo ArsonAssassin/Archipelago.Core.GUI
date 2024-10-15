9ea5a47c89264a3e10e66255c2fd79a2cd7a218f
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2024-10-15 11:36:23 +0100

    SetLogLevel option

--- Logging/ArchipelagoGuiSink.cs
+++ Logging/ArchipelagoGuiSink.cs
@@ -12,22 +12,27 @@ namespace Archipelago.Core.GUI.Logging
 {
     public class ArchipelagoGuiSink : ILogEventSink
     {
+        private LogEventLevel _logLevel;
         private Action<string> _outputEvent;
-        public ArchipelagoGuiSink(Action<string> outputEvent)
+        public ArchipelagoGuiSink(Action<string> outputEvent, LogEventLevel level = LogEventLevel.Information)
         {
+            _logLevel = level;
             _outputEvent = outputEvent;
         }
         public void Emit(LogEvent logEvent)
         {
-            _outputEvent?.Invoke(logEvent.RenderMessage());
+            if(logEvent.Level <= _logLevel)
+            {
+                _outputEvent?.Invoke(logEvent.RenderMessage());
+            }
         }
     }
     public static class ArchipelagoGuiSinkExtensions
     {
         public static LoggerConfiguration ArchipelagoGuiSink(
-                  this LoggerSinkConfiguration loggerConfiguration, Action<string> outputEvent)
+                  this LoggerSinkConfiguration loggerConfiguration, Action<string> outputEvent, LogEventLevel level = LogEventLevel.Information)
         {
-            return loggerConfiguration.Sink(new ArchipelagoGuiSink(outputEvent));
+            return loggerConfiguration.Sink(new ArchipelagoGuiSink(outputEvent, level));
         }
     }
 }
--- Logging/LoggerConfig.cs
+++ Logging/LoggerConfig.cs
@@ -1,4 +1,6 @@
 ﻿using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +12,24 @@ namespace Archipelago.Core.GUI.Logging
     public static class LoggerConfig
     {
         private static ILogger _logger;
+        private static Action<string> _outputAction;
 
         public static void Initialize(Action<string> mainFormWriter)
         {
+            _outputAction = mainFormWriter;
             var loggerConfiguration = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.ArchipelagoGuiSink(mainFormWriter);
+                .WriteTo.ArchipelagoGuiSink(_outputAction);
 
             _logger = loggerConfiguration.CreateLogger();
             Log.Logger = _logger;
         }
-
+        public static void SetLogLevel(LogEventLevel level)
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .WriteTo.ArchipelagoGuiSink(_outputAction, level);
+            _logger = loggerConfiguration.CreateLogger();
+            Log.Logger = _logger;
+        }
         public static LoggerConfiguration GetLoggerConfiguration(Action<string> mainFormWriter)
         {
             return new LoggerConfiguration()
